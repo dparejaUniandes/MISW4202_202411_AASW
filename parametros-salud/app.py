b99cd20dafe516a2a81d1dc88c744ddc5fe64d7e
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2024-02-25 17:35:26 -0500

    se agrega deteccion de fallas por parametro en post

--- parametros-salud/app.py
+++ parametros-salud/app.py
@@ -20,12 +20,12 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Dato de salud recibido</p>"
 
-def enviar_señales_alive(duracion_experimento, porcentaje_falla):
+def enviar_señales_alive(duracion_experimento, porcentaje_falla, duracion_falla):
     inicio_experimento = time.time()
     while time.time() - inicio_experimento < duracion_experimento:
         if random.random() < porcentaje_falla / 100:  # Probabilidad de falla
             logging.info("Simulando falla en el envío de señales de monitoreo")
-            time.sleep(2)  # Simula una falla de 0.5 segundos
+            time.sleep(duracion_falla)  # Simula una falla de 0.5 segundos
             continue  # Salta al siguiente ciclo sin enviar señales
         # Obtiene la fecha y hora actual con 3 decimales de precisión
         fecha_hora_actual = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
@@ -36,18 +36,18 @@ def enviar_señales_alive(duracion_experimento, porcentaje_falla):
         time.sleep(0.1)
     fecha_hora_actual = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
-def start_heartbeat_thread(duracion_experimento, porcentaje_falla):
-    heartbeat_thread = threading.Thread(target=enviar_señales_alive, args=(duracion_experimento, porcentaje_falla,))
+def start_heartbeat_thread(duracion_experimento, porcentaje_falla, duracion_falla):
+    heartbeat_thread = threading.Thread(target=enviar_señales_alive, args=(duracion_experimento, porcentaje_falla, duracion_falla))
     heartbeat_thread.start()
 
 
 # Ruta para iniciar el experimento
 @app.route("/iniciar_experimento", methods=["POST"])
 def iniciar_experimento():
-    fecha_hora_actual = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
     duracion_experimento = request.json.get("duracion_experimento", 100)  # Duración predeterminada de 100 segundos
     porcentaje_falla = request.json.get("porcentaje_falla", 5)  # Porcentaje de falla predeterminado de 5%
-    start_heartbeat_thread(duracion_experimento, porcentaje_falla)
+    duracion_falla = request.json.get("duracion_falla", 2)  # Duración de la falla predeterminada de 2 segundos
+    start_heartbeat_thread(duracion_experimento, porcentaje_falla, duracion_falla)
     return jsonify({"mensaje": "Experimento iniciado correctamente"})
 
 if __name__ == "__main__":
